fa0c102542525f78953caaa21cb5ca6da9d4b7a3
conor-mcgowan <conor.mcgowan@live.com>
2021-03-03 14:39:57 -0600

    adds env data and server.js prelim architecture

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("This is a server, apparently."));
+
+app.get("*", (req, res) res.redirect("/"));
+
+app.listen(port, () => console.log(`The doors are open on port: ${PORT}`));
